d2911354afebbbcc81eccbfc30d7ae5a3b6bf432
Egor Moskaliov <58050140+duckbilled395@users.noreply.github.com>
2022-07-28 16:06:53 +0300

    dev validation

---
+++ src/shared/NameInput/NameInput.tsx
@@ -0,0 +1,23 @@
+import React, {FC, FormEvent} from 'react';
+
+type PropsType = {
+    name: string,
+    setName: (name: string) => void,
+    nameDirty: boolean,
+    nameError: string,
+    blurHandler: (e: FormEvent<HTMLFormElement> | FormEvent<HTMLInputElement>) => void
+}
+
+const Name: FC<PropsType> = ({name, setName, nameDirty, nameError, blurHandler}) => {
+    console.log('render name')
+    return (
+        <>
+            {(nameDirty && nameError) && <div>{nameError}</div>}
+            <input className='nameField' onBlur={e => blurHandler(e)} value={name}
+                   onChange={(e) => setName(e.target.value)} name='name' type='text'
+                   placeholder='Name and surname'/>
+        </>
+    );
+};
+
+export const NameInput = React.memo(Name);
